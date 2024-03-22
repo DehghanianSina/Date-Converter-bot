943a7dc587f6be8b4655593d460677ff046919cc
Sina Dehghanian <Dehghanian.Sina@gmail.com>
2024-03-23 01:18:38 +0330

    modify output printing

--- bot.py
+++ bot.py
@@ -3,41 +3,68 @@ from dateutil import parser
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
+import datetime 
 from khayyam import JalaliDatetime, JalaliDate as GregorianDatetime
 
+LRM = '\u200E'
+RLM = '\u200F'
+footer = '''
+\-\-\-
+@myDateConverterBot
+'''
 
 def convert_date_text(input_date):
     try:
-        # Extract year from the input using regex
-        year_match = re.search(r'\b\d{2,4}\b', input_date)
-        if year_match:
-            year = int(year_match.group())
-            
-            # Determine likely era based on the year
-            era = "Jalali" if 1200 <= year <= 1500 else "Gregorian" if 1900 <= year <= 2100 else None
+        # Extract year, month, and day using regex
+        matches = re.findall(r'\b\d{1,4}\b', input_date)
+        if len(matches) != 3:
+            raise ValueError("Invalid date format")
 
-            if era == "Jalali":
-                # Try parsing as Jalali date using khayyam
-                jalali_datetime = JalaliDatetime.strptime(input_date, '%Y/%m/%d')
-                
-                # Convert to Gregorian date
-                gregorian_date = jalali_datetime.todate()
-                gregorian_formatted = gregorian_date.strftime('%Y-%m-%d')
+        year, month, day = map(int, matches)
 
-                return f'''*input* \({era}\):\n`{input_date}`\n\n*output* \(Gregorian\):\n`{gregorian_formatted}`\n{gregorian_date.strftime("%B")}'''
-            
-            # f'{input_date} \({era}\) \=\> `{gregorian_formatted}` \(Gregorian\)\n{gregorian_date.strftime("%B")}'
-            
-            elif era == "Gregorian":
-                # Try parsing as Gregorian date
-                parsed_gregorian_date = parser.parse(input_date)
-                
-                # Convert to Jalali date
-                jalali_date = JalaliDatetime(parsed_gregorian_date)
-                jalali_formatted = jalali_date.strftime('%Y/%m/%d')
+        # Determine likely era based on the year
+        era = "Jalali" if 1200 <= year <= 1500 else "Gregorian" if 1900 <= year <= 2100 else None
 
-                #return f'\{input_date\} \({era}\) => {jalali_formatted} \(Jalali\)'
-                return f'''*input* \({era}\):\n`{input_date}`\n\n*output* \(Jalali\):\n`{jalali_formatted}`\n{jalali_date.strftime("%B")}'''
+        if era == "Jalali":
+            # Try parsing as Jalali date using khayyam
+            jalali_datetime = JalaliDatetime(year, month, day)
+
+            # Convert to Gregorian date
+            gregorian_date = jalali_datetime.todate()
+            gregorian_formatted = gregorian_date.strftime('%Y-%m-%d')
+            
+            output = f'''*input* \({era}\):\n`{jalali_datetime.strftime('%Y-%m-%d')}`\n\n*output* \(Gregorian\):\n`{gregorian_formatted}`\n{gregorian_date.strftime("%B")}
+
+            `{jalali_datetime.strftime("%A")}, {jalali_datetime.strftime("%d")} {jalali_datetime.strftime("%B")} {jalali_datetime.strftime("%Y")}`
+
+`{gregorian_date.strftime("%A")}, {gregorian_date.strftime("%d")} {gregorian_date.strftime("%B")} {gregorian_date.strftime("%Y")}`
+            
+            {footer}
+            '''
+
+            return output
+
+        elif era == "Gregorian":
+            # Try parsing as Gregorian date
+            gregorian_date = datetime.datetime.strptime(f'{year}-{month}-{day}', '%Y-%m-%d')
+            #parser.parse(input_date)
+
+            # Convert to Jalali date
+            jalali_date = JalaliDatetime(gregorian_date)
+            jalali_formatted = jalali_date.strftime('%Y/%m/%d')
+
+            #return f'''*input* \({era}\):\n`{gregorian_date.strftime('%Y-%m-%d')}`\n\n*output* \(Jalali\):\n`{jalali_formatted}`\n{jalali_date.strftime("%B")}'''
+        
+            output = f'''*input* \({era}\):\n`{gregorian_date.strftime('%Y-%m-%d')}`\n\n*output* \(Jalali\):\n`{jalali_formatted}`\n{jalali_date.strftime("%B")}
+            
+            `{gregorian_date.strftime("%A")}, {gregorian_date.strftime("%d")} {gregorian_date.strftime("%B")} {gregorian_date.strftime("%Y")}`
+            
+`{jalali_date.strftime("%A")}, {jalali_date.strftime("%d")} {jalali_date.strftime("%B")} {jalali_date.strftime("%Y")}`
+            
+            {footer}
+            '''
+
+            return output
 
         return "Invalid date format. Please use a valid date format."
 
